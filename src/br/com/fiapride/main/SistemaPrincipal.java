afe706873080747845795da74051dec534631232
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-24 10:34:59 -0300

    feat: adiciona métodos de comportamento e atualiza README

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -1,19 +1,46 @@
 package br.com.fiapride.main;
+
 import br.com.fiapride.model.Fogao;
+
 public class SistemaPrincipal {
 
 	public static void main(String[] args) {
-		Fogao meuFogao= new Fogao();
-		meuFogao.tamanho="pequeno";
-		meuFogao.embutido=false;
-		
-		Fogao professorFogao=new Fogao();
-		professorFogao.tamanho="grande";
-		professorFogao.embutido=true;
-		System.out.println("O tamanho do meu fogão é " + meuFogao.tamanho );
-		System.out.println("Meu fogão é embutido? " + meuFogao.embutido);
-		System.out.println("O tamanho do fogão do professor é " + professorFogao.tamanho );
-		System.out.println("O fogão do professor é embutido? " + professorFogao.embutido);
-	}
 
+		Fogao meuFogao = new Fogao();
+
+		meuFogao.tamanho = "pequeno";
+		meuFogao.ligado = false;
+		meuFogao.quantidade_bocas = 4;
+
+		System.out.println("O tamanho do meu fogão é " + meuFogao.tamanho);
+		System.out.println("Meu fogão está ligado? " + meuFogao.ligado);
+
+		// Teste válido: ligar o fogão
+		meuFogao.ligar_fogao();
+
+		// Teste inválido: tentar ligar novamente
+		meuFogao.ligar_fogao();
+
+		// Teste válido: acender 2 bocas
+		meuFogao.ligar_bocas(2);
+
+		// Teste inválido: o fogão possui apenas 4 bocas
+		meuFogao.ligar_bocas(5);
+
+		// Teste inválido: quantidade menor ou igual a zero
+		meuFogao.ligar_bocas(0);
+
+		System.out.println("Bocas acesas: " + meuFogao.bocas_acesas);
+
+		Fogao professorFogao = new Fogao();
+
+		professorFogao.tamanho = "grande";
+		professorFogao.ligado = true;
+		professorFogao.quantidade_bocas = 6;
+
+		System.out.println("O tamanho do fogão do professor é "
+				+ professorFogao.tamanho);
+		System.out.println("O fogão do professor está ligado? "
+				+ professorFogao.ligado);
+	}
 }
--- src/br/com/fiapride/model/Fogao.java
+++ src/br/com/fiapride/model/Fogao.java
@@ -1,6 +1,29 @@
 package br.com.fiapride.model;
 
 public class Fogao {
+
 	public String tamanho;
-	public boolean embutido;
+	public boolean ligado;
+	public int quantidade_bocas;
+	public int bocas_acesas;
+
+	public void ligar_fogao() {
+		if (ligado) {
+			System.out.println("O seu fogão já está ligado!");
+		} else {
+			ligado = true;
+			System.out.println("Fogão ligado!");
+		}
+	}
+
+	public void ligar_bocas(int bocas_acessas) {
+		if (!ligado) {
+			System.out.println("Não é possível acender as bocas com o fogão desligado!");
+		} else if (bocas_acessas <= 0 || bocas_acessas > quantidade_bocas) {
+			System.out.println("Quantidade de bocas inválida!");
+		} else {
+			this.bocas_acesas = bocas_acessas;
+			System.out.println("Foram acendidas " + bocas_acessas + " bocas.");
+		}
+	}
 }
