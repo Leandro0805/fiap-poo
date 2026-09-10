4b56b92496feb82f7c5eaee13ddf2753b375b9ee
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-10 09:19:54 -0300

    refactor: aplica encapsulamento na classe Passageiro e Objeto Pessoal

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -4,43 +4,54 @@ import br.com.fiapride.model.Fogao;
 
 public class SistemaPrincipal {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
+	
+	//Definição do objeto e dos seus atributos
+	Fogao meuFogao = new Fogao();
 
-		Fogao meuFogao = new Fogao();
+	meuFogao.setTamanho("pequeno");
+	meuFogao.setLigado(false);
+	meuFogao.setQuantidade_bocas(4);
+	
+	//Teste dos atributos 
+	System.out.println("O tamanho do meu fogão é " + meuFogao.getTamanho());
+	System.out.println("Meu fogão está ligado? " + meuFogao.isLigado());
 
-		meuFogao.tamanho = "pequeno";
-		meuFogao.ligado = false;
-		meuFogao.quantidade_bocas = 4;
+	//Testando as função ligar_fogao
+	
+	// Teste válido: ligar o fogão
+	meuFogao.ligar_fogao();
 
-		System.out.println("O tamanho do meu fogão é " + meuFogao.tamanho);
-		System.out.println("Meu fogão está ligado? " + meuFogao.ligado);
+	// Teste inválido: tentar ligar novamente
+	meuFogao.ligar_fogao();
 
-		// Teste válido: ligar o fogão
-		meuFogao.ligar_fogao();
+	
+	//Testando a função ligar_bocas
+	// Teste válido: acender 2 bocas
+	meuFogao.ligar_bocas(2);
 
-		// Teste inválido: tentar ligar novamente
-		meuFogao.ligar_fogao();
+	// Teste inválido: o fogão possui apenas 4 bocas
+	meuFogao.ligar_bocas(5);
 
-		// Teste válido: acender 2 bocas
-		meuFogao.ligar_bocas(2);
+	// Teste inválido: quantidade menor ou igual a zero
+	meuFogao.ligar_bocas(0);
 
-		// Teste inválido: o fogão possui apenas 4 bocas
-		meuFogao.ligar_bocas(5);
+	//Quantidade de bocas acesas
+	System.out.println("Bocas acesas: " + meuFogao.getBocas_acesas());
+	
+	//Objeto professorFogao
+	Fogao professorFogao = new Fogao();
 
-		// Teste inválido: quantidade menor ou igual a zero
-		meuFogao.ligar_bocas(0);
+	professorFogao.setTamanho("grande");
+	professorFogao.setLigado(true);
+	professorFogao.setQuantidade_bocas(6);
 
-		System.out.println("Bocas acesas: " + meuFogao.bocas_acesas);
+	System.out.println("O tamanho do fogão do professor é "
+			+ professorFogao.getTamanho());
 
-		Fogao professorFogao = new Fogao();
+	System.out.println("O fogão do professor está ligado? "
+			+ professorFogao.isLigado());
+}
 
-		professorFogao.tamanho = "grande";
-		professorFogao.ligado = true;
-		professorFogao.quantidade_bocas = 6;
 
-		System.out.println("O tamanho do fogão do professor é "
-				+ professorFogao.tamanho);
-		System.out.println("O fogão do professor está ligado? "
-				+ professorFogao.ligado);
-	}
 }
--- src/br/com/fiapride/model/Fogao.java
+++ src/br/com/fiapride/model/Fogao.java
@@ -2,28 +2,82 @@ package br.com.fiapride.model;
 
 public class Fogao {
 
-	public String tamanho;
-	public boolean ligado;
-	public int quantidade_bocas;
-	public int bocas_acesas;
+    private String tamanho;
+    private boolean ligado;
+    private int quantidade_bocas;
+    private int bocas_acesas;
 
-	public void ligar_fogao() {
-		if (ligado) {
-			System.out.println("O seu fogão já está ligado!");
-		} else {
-			ligado = true;
-			System.out.println("Fogão ligado!");
-		}
-	}
+    // Get e set de tamanho
+    public String getTamanho() {
+        return tamanho;
+    }
 
-	public void ligar_bocas(int bocas_acessas) {
-		if (!ligado) {
-			System.out.println("Não é possível acender as bocas com o fogão desligado!");
-		} else if (bocas_acessas <= 0 || bocas_acessas > quantidade_bocas) {
-			System.out.println("Quantidade de bocas inválida!");
-		} else {
-			this.bocas_acesas = bocas_acessas;
-			System.out.println("Foram acendidas " + bocas_acessas + " bocas.");
-		}
-	}
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    // Get e set de ligado
+    public boolean isLigado() {
+        return ligado;
+    }
+
+    public void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    // Get e set de quantidade_bocas
+    public int getQuantidade_bocas() {
+        return quantidade_bocas;
+    }
+
+    public void setQuantidade_bocas(int quantidade_bocas) {
+        if (quantidade_bocas > 0) {
+            this.quantidade_bocas = quantidade_bocas;
+        } else {
+            System.out.println("Quantidade de bocas inválida!");
+        }
+    }
+
+    // Get e set de bocas_acesas
+    public int getBocas_acesas() {
+        return bocas_acesas;
+    }
+
+    public void setBocas_acesas(int bocas_acesas) {
+        if (bocas_acesas >= 0 && bocas_acesas <= this.quantidade_bocas) {
+            this.bocas_acesas = bocas_acesas;
+        } else {
+            System.out.println("Quantidade de bocas acesas inválida!");
+        }
+    }
+
+    // Função para ligar o fogão
+    public void ligar_fogao() {
+        if (ligado) {
+            System.out.println("O seu fogão já está ligado!");
+        } else {
+            ligado = true;
+            System.out.println("Fogão ligado!");
+        }
+    }
+
+    // Função para ligar uma quantidade de bocas
+    public void ligar_bocas(int bocas_acessas) {
+
+        if (!ligado) {
+            System.out.println(
+                "Não é possível acender as bocas com o fogão desligado!"
+            );
+
+        } else if (bocas_acessas <= 0 || bocas_acessas > quantidade_bocas) {
+            System.out.println("Quantidade de bocas inválida!");
+
+        } else {
+            setBocas_acesas(bocas_acessas);
+
+            System.out.println(
+                "Foram acendidas " + bocas_acessas + " bocas."
+            );
+        }
+    }
 }
